215d05bbce4714b3a38a15a711df1b09222ef73a
Diego Gimbernat <gimbernat13@gmail.com>
2022-10-16 16:58:55 -0500

    token table works

--- src/views/Tokens/index.jsx
+++ src/views/Tokens/index.jsx
@@ -2,17 +2,29 @@ import { useQuery } from "@apollo/client";
 import React from "react";
 import { Switch } from "react-router-dom";
 import { RouteWithSubRoutes } from "../..";
+import { Button } from "../../components/Button/Button";
 import { FilterButtons } from "../../components/FilterButtons/FilterButtons";
 import { Loader } from "../../components/Loader/Loader";
 import { TokenCardList } from "../../components/TokenCardList/TokenCardList";
 import { TokenTable } from "../../components/TokenTable/TokenTable";
 import { TOKENS } from "./queries";
+import * as Styled from "./styles";
 
 export function TokensView({ routes }) {
   // FIXME: ADD TIME FILTER TO CHARTS
   const [numberOfDays, setNumberOfDays] = React.useState({
     value: 50,
     name: 50,
+  });
+  const viewTypes = [
+    { id: "table", text: "Table", icon: "" },
+    { id: "cards", text: "Cards", icon: "" },
+  ];
+
+  const [viewType, setViewType] = React.useState({
+    id: "table",
+    text: "Table",
+    icon: "",
   });
 
   const { loading, error, data } = useQuery(TOKENS);
@@ -21,17 +33,32 @@ export function TokensView({ routes }) {
   console.log("table data ", data);
   return (
     <div>
-      <h3>Tokens</h3>
+      <h2>Tokens</h2>
       <Switch>
         {routes.map((route, i) => (
           <RouteWithSubRoutes key={i} {...route} />
         ))}
       </Switch>
 
-      <FilterButtons />
-      <TokenTable tableData={data.tokens} />
+      <Styled.FlexSpaced>
+        <FilterButtons />
+        <h3>Most Traded</h3>
+        <div>
+          {viewTypes.map((type) => {
+            return (
+              <Button
+                isActive={viewType.id === type.id}
+                onClick={() => setViewType(type)}
+              >
+                {type.text}
+              </Button>
+            );
+          })}
+        </div>
+      </Styled.FlexSpaced>
 
-      <TokenCardList />
+      {viewType.id === "table" && <TokenTable tableData={data.tokens} />}
+      {viewType.id === "cards" && <TokenCardList />}
     </div>
   );
 }
