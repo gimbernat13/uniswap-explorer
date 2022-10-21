543f9d2cffa9b2f618c058f185b8291a089ff705
Diego Gimbernat <gimbernat13@gmail.com>
2022-10-21 01:57:23 -0500

    Update PairCard.jsx

--- src/components/atomic/molecules/PairCard/PairCard.jsx
+++ src/components/atomic/molecules/PairCard/PairCard.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import formatNumber from "../../../../utils/formatNumber";
 import { Card } from "../../atoms/Card/Card";
 import * as Styled from "./styles";
-
+import { UilExchange } from "@iconscout/react-unicons";
+import { Button } from "components/atomic/atoms/Button/Button";
 export const PairCard = ({ pair }) => {
   const animatedItem = {
     hidden: { opacity: 0 },
@@ -18,6 +19,10 @@ export const PairCard = ({ pair }) => {
             </div>
             <div>{pair.txCount} Tx's</div>
             <div>${formatNumber(parseInt(pair.volumeUSD))}</div>
+            <Button>
+              Trade
+              <UilExchange size={15} />
+            </Button>
           </Styled.PoolCardInner>
         </Styled.PoolCard>
       </Card>
